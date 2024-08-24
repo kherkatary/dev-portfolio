aa271028a6de76e43f888bf0b87b5a162374f4d6
Kherkatary <kherkatarysan456@gmail.com>
2024-08-24 22:54:16 +0530

    image change in about.tsx

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -10,7 +10,7 @@ const About = () => {
 
       <div className={styles.gridContainer}>
 
-        <div className={styles.imageHolder}><img src="/human1-Astro.png" alt="Its me" /></div>
+        <div className={styles.imageHolder}><img src="/human8.png" alt="Its me" /></div>
         <div className={styles.content}>
           I’m Sandeep Kherkatary, a software developer and Computer Science graduate from NIT Silchar. I specialize in React, Node.js, MongoDB, and C++, focusing on creating efficient and scalable solutions. I’ve led projects like "Nurture Mental Health" and have been actively involved in college clubs, which has sharpened my skills and passion for technology.
           <br /><br />I enjoy solving complex problems and optimizing code, all while adding a bit of humor to the process. Always eager to explore new technologies, I strive to bring creativity and dedication to every project I work on.
